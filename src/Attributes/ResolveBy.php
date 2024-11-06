d0b273fc2d8db1e0b2f766fcb38030d2cf52230a
Yoshiyuki Yamato <81786746+d1vby0@users.noreply.github.com>
2024-11-06 10:13:10 +0900

    first commit

---
+++ src/Attributes/ResolveBy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jorro\Reflective\Attributes;
+
+#[\Attribute]
+class ResolveBy
+{
+    public function __construct()
+    {
+    }
+
+    public function getId(): ?string
+    {
+        return null;
+    }
+
+    public function getValues(): ?array
+    {
+        return null;
+    }
+}
